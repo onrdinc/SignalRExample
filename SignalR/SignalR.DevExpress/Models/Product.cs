1bb9b1a8f3bce5af8dd6517c148d149946087ca2
Onur DİNÇ <103895148+onrdinc@users.noreply.github.com>
2023-11-23 17:54:49 +0300

    .

---
+++ SignalR/SignalR.DevExpress/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace SignalR.DevExpress.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+        public decimal OpenPrice { get; set; }
+        public decimal Change
+        {
+            get
+            {
+                return Price - OpenPrice;
+            }
+        }
+    }
+}
